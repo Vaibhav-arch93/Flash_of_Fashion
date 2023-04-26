42645e59bf55234b73af4715674c28b7236a298c
Vaibhav-arch93 <79572362+Vaibhav-arch93@users.noreply.github.com>
2023-04-27 02:40:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+/* start server */
+	server.listen(PORT, err => {
+		if (err) return console.log('something bad happened', err.message);
+		console.log(`server is listening on ${PORT}`);
+	});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
